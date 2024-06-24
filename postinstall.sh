ffc44ceac321452edacc70bce72c9377c812f538
Phowobos <155765965+phowobos@users.noreply.github.com>
2024-06-24 12:04:41 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -16,12 +16,12 @@ echo Ubuntu-Laptop-$(openssl rand -hex 3) > /etc/hostname
 sudo mkdir /usr/share/wallpapers
 sudo wget https://raw.githubusercontent.com/phowobos/Minecraftcamp-Ubuntu-Autoinstall/main/Minecraft_Background.png -P /usr/share/wallpapers
 
-cat >> /etc/dconf/profile/user << EOF
+sudo cat >> /etc/dconf/profile/user << EOF
 user-db:user
 system-db:local
 EOF
 
-cat >> /etc/dconf/db/local.d/00-background << EOF
+sudo cat >> /etc/dconf/db/local.d/00-background << EOF
 # Specify the dconf path
 [org/gnome/desktop/background]
 
@@ -38,7 +38,7 @@ primary-color='000000'
 secondary-color='FFFFFF'
 EOF
 
-cat >> /etc/dconf/db/local.d/locks/background << EOF
+sudo cat >> /etc/dconf/db/local.d/locks/background << EOF
 # Lock desktop background settings
 /org/gnome/desktop/background/picture-uri
 /org/gnome/desktop/background/picture-options
